3e1297d07a7eceddd7e5824c20d5b9b86bd728c2
Melissa Heying <melissakheying@gmail.com>
2020-05-08 15:59:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,3 +5,15 @@ function makeInt(string) {
 function preserveDecimal(string) {
   parseFloat('string', 10);
 }
+
+add(string + 10)
+/*subtract(string - 10)
+multiply(string * 10)
+divide (string / 10)
+add(a,b)
+subtract(a,b)
+multiply(a,b)
+divide(a,b)
+increment(n)
+decrement(n)
+makeInt(n)*/
